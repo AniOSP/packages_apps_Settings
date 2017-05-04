24b89b6e16467372f439ce228968f20c7b74e6c8
Jackeagle <jackeagle102@gmail.com>
2017-05-04 13:36:21 -0400

    Settings: Integrate Caster into Settings
    - Adapt to Android R Changes
    - Keep it below Connected Devices
    
    Change-Id: I30a65c3e17aff251eef6bbf2454203bbed3300ae
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Daryl Ronningen <relms@relms.dev>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -324,4 +324,9 @@ public class Settings extends SettingsActivity {
      * Activity for AppDashboard.
      */
     public static class AppDashboardActivity extends SettingsActivity {}
+
+    /**
+     * Activity for Caster
+     */
+    public static class AniospSettingsActivity extends SettingsActivity {}
 }
